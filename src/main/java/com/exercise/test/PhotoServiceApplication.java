673ab10e0c67802043587dfa43bdc71c004be434
Arif Shaikh <SYSDEVA5@morrisonsplc.co.uk>
2016-10-12 14:05:21 +0100

    Main entry class for the service

---
+++ src/main/java/com/exercise/test/PhotoServiceApplication.java
@@ -0,0 +1,64 @@
+package com.exercise.test;
+
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+/**
+ * Created by sysdeva5 on 12/10/2016.
+ *
+ * This is the main entry class for this photo service.
+ */
+public class PhotoServiceApplication extends Application<PhotoConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new PhotoServiceApplication().run(args);
+    }
+
+    /**
+     *
+     * @return - application name
+     *
+     * This method defines the name of the appliaction
+     */
+    @Override
+    public String getName() {
+        return "photo-service";
+    }
+
+    /**
+     *
+     * @param bootstrap - Bootstrap object
+     *
+     * This method bootstraps the appliaction
+     */
+    @Override
+    public void initialize(Bootstrap<PhotoConfiguration> bootstrap) {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(
+                        bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
+
+        bootstrap.addBundle(new AssetsBundle());
+    }
+
+    /**
+     *
+     * @param configuration - Dropwizard configuration object
+     * @param environment - Dropwizard environment object
+     *
+     * This method is run after Bundles are run. Ressource are registered here.
+     */
+    @Override
+    public void run(PhotoConfiguration configuration, Environment environment) {
+
+        final String template = configuration.getTemplate();
+        final String defaultName = configuration.getDefaultName();
+
+    }
+}
