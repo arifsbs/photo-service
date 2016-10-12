b27f5b1eec3007462aa3380315923bc272735daa
Arif Shaikh <SYSDEVA5@morrisonsplc.co.uk>
2016-10-12 14:21:10 +0100

    Added photo album resource exposed by this service

---
+++ src/main/java/com/exercise/test/PhotoResource.java
@@ -0,0 +1,22 @@
+package com.exercise.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * Created by sysdeva5 on 12/10/2016.
+ */
+
+@Path("/photo-album")
+@Produces(MediaType.APPLICATION_JSON)
+public class PhotoResource {
+
+    @GET
+    public List getImages() {
+
+        return null;
+    }
+}
