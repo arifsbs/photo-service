6a23cda8e8c2f18ce42c9178085e9c5d48a12662
Arif Shaikh <SYSDEVA5@morrisonsplc.co.uk>
2016-10-12 14:29:41 +0100

    Added jersey client property to make HTTP calls to Flickr service

--- src/main/java/com/exercise/test/resources/PhotoResource.java
+++ src/main/java/com/exercise/test/resources/PhotoResource.java
@@ -5,6 +5,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by sysdeva5 on 12/10/2016.
@@ -14,9 +18,15 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class PhotoResource {
 
+    private Client client;
+
+    public PhotoResource(Client client) {
+        this.client = client;
+    }
+
     @GET
     public List getImages() {
-
+        
         return null;
     }
 }
