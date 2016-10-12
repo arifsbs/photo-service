dfb31d6e3d3fece2ec308a88f45965bbf827c3db
Arif Shaikh <SYSDEVA5@morrisonsplc.co.uk>
2016-10-12 14:50:05 +0100

    Classes which are JSON representation for the data returned by calling Flickr's public feed service

---
+++ src/main/java/com/exercise/test/entity/Media.java
@@ -0,0 +1,20 @@
+package com.exercise.test.entity;
+
+/**
+ *  Created by Arif Shaikh on 12/10/2016.
+ *
+ * This class is the JSON representation for Media property part of the each image entry
+ * returned by calling Flickr's public feed service
+ */
+public class Media {
+
+    private String m;
+
+    public String getM() {
+        return m;
+    }
+
+    public void setM(String m) {
+        this.m = m;
+    }
+}
